c6311b6df100c31c1a9f09593fba000b7a64f759
achatrian <chatrian.andrea@gmail.com>
2019-09-19 13:19:40 +0100

    updated demo with label_options

--- aida_convert/demo.py
+++ aida_convert/demo.py
@@ -21,19 +21,36 @@ label_value_map = {
                 'epithelium': 200,
                 'lumen': 250,
                 'background': 0
-            }
+}
 # Interval value for each class (i.e. if ground truth is not perfect)
 label_interval_map = {
             'epithelium': (31, 225),
             'lumen': (225, 250),
             'background': (0, 30)
-        }
+}
+
+# Options to process each label:
+label_options = {
+            'epithelium': {
+                'small_object_size': 1024*0.4,
+                'dist_threshold': 0.05,
+                'final_closing_size': 20,
+                'final_dilation_size': 2
+            },
+            'lumen': {
+                'small_object_size': 400,
+                'dist_threshold': 0.001,
+                'final_closing_size': 15,
+                'final_dilation_size': 5
+            }
+}
 
 converter = MaskConverter(
-        value_hier=value_hier,
-        label_value_map=label_value_map,
-        label_interval_map=label_interval_map,
-    )
+    value_hier=value_hier,
+    label_value_map=label_value_map,
+    label_interval_map=label_interval_map,
+    label_options=label_options
+)
 
 my_classes = ['epithelium', 'lumen', 'background']
 annotation = AnnotationBuilder('slide_1', 'my_project', my_classes)
